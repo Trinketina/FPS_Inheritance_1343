142c2c32f49b037264fa45ab64f01a05d0cdd3d1
Tina <143371814+Trinketina@users.noreply.github.com>
2024-04-12 01:05:08 -0500

    modified SamplePosition
    adjusted SamplePosition to be based on the scale of Jeff, since he was having issues realigning to the NavMesh when too large or small.
    making him smaller than .2f roughly still causes issues with the NavAgent, not sure how to solve though.
    
    Additionally, added a timeout length to the Recovery(), so that if Jeff takes too long to realign to the NavMesh and Re-enable, he just respawns.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -29,6 +29,8 @@ public class Enemy : MonoBehaviour
     FPSController player;
 
     float elapsed = 0;
+    float maxDist = 1;
+    float MaxDist { get { return maxDist; } set { maxDist = (value > 5) ? value : 5; } }
 
     // Start is called before the first frame update
     void Start()
@@ -81,16 +83,20 @@ public class Enemy : MonoBehaviour
     {
         if (CheckRange(player.transform.position, agent.transform.position, pursueRange))
         {
+            //starts to chase player if within range
             ChangeState(EnemyMovementStates.Pursue, 0);
             return;
         }
         if (elapsed >= recoveryTime)
         {
+            //otherwise wander within a range
             float randX = Random.Range(-wanderRange, +wanderRange);
             float randZ = Random.Range(-wanderRange, +wanderRange);
             Vector3 wanderPos = new Vector3(origin.x + randX, origin.y, origin.z + randZ);
+
             NavMeshHit hit;
-            bool gotHit = NavMesh.SamplePosition(wanderPos, out hit, 2, NavMesh.AllAreas);
+            maxDist = transform.localScale.y * 2;
+            bool gotHit = NavMesh.SamplePosition(wanderPos, out hit, maxDist, NavMesh.AllAreas);
             if (gotHit)
             {
                 agent.SetDestination(hit.position);
@@ -104,15 +110,18 @@ public class Enemy : MonoBehaviour
         Vector3 playerPos = player.transform.position;
         if (CheckRange(playerPos, agent.transform.position, attackRange))
         {
+            //if close enough, attack
             ChangeState(EnemyMovementStates.Attack, 0);
             return;
         }
         if (CheckRange(playerPos, agent.transform.position, pursueRange))
         {
+            //otherwise keep pursuing
             agent.SetDestination(playerPos);
         }
         else
         {
+            //if out of range, switch to wander
             ChangeState(EnemyMovementStates.Wander, recoveryTime);
         }
         
@@ -125,67 +134,81 @@ public class Enemy : MonoBehaviour
         Damager.SetActive(true);
 
         Rigidbody.AddForce((player.transform.position - agent.transform.position).normalized * attackMagnitude, ForceMode.VelocityChange);
+        //Rigidbody.velocity += (player.transform.position - agent.transform.position).normalized * attackMagnitude;
 
-
+        //finished attack, move to recovery
         ChangeState(EnemyMovementStates.Recovery, 0);
     }
     void Recovery()
     {
         if (elapsed > recoveryTime/2)
         {
-            bool isSpawnable = NavMesh.SamplePosition(agent.transform.position, out NavMeshHit hit, gameObject.transform.localScale.y, NavMesh.AllAreas);
+            //check to make sure the agent is above working NavMesh before reenabling
+            maxDist = transform.localScale.y * 2;
+            bool isSpawnable = NavMesh.SamplePosition(agent.transform.position, out NavMeshHit hit, MaxDist, NavMesh.AllAreas);
             if (isSpawnable)
             {
                 agent.enabled = true;
                 Rigidbody.isKinematic = true;
                 Damager.SetActive(false);
             }
+            else if (elapsed > recoveryTime*2) 
+            {
+                //if Enemy gets stuck and can't find a safe place to spawn, it respawns
+                Respawn();
+                return;
+            } 
             else { return; }
         }
         if (elapsed >= recoveryTime && agent.enabled)
         {
+            //if successfully reenabled, start to wander
             ChangeState(EnemyMovementStates.Wander, recoveryTime);
         }
     }
 
     void ReEnableAI()
     {
-        Rigidbody.isKinematic = false;
+        //when disabled, it will try to reenable
         if (elapsed > .1f)
         {
             if (Rigidbody.velocity.magnitude <= 0f && 
                 Rigidbody.constraints != RigidbodyConstraints.FreezeAll)
             {
                 //enable the AI
+                //enter the recovery state with full recoveryTime so it immediately tries to re-enable navAgent
                 ChangeState(EnemyMovementStates.Recovery, recoveryTime);
-                Debug.Log("Nav Agent Re-enabled");
+                //Debug.Log("Nav Agent Re-enabled");
                 return;
             }
         }
-        if (elapsed > 7f)
+        if (elapsed > recoveryTime*2)
         {
+            //this is for when the enemy has been frozen by the Physgun, allowing it to break free after double the length of recovery
             Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
             Rigidbody.useGravity = true;
             ResetTime();
             return;
         }
     }
-    public void DisableAI()
+    public void DisableAI() //is called when hit by attack
     {
         if (moveState != EnemyMovementStates.Disabled)
         {
             //disable the AI
             agent.enabled = false;
             Rigidbody.isKinematic = false;
-            Debug.Log("Nav Agent Disabled");
+            //Debug.Log("Nav Agent Disabled");
             ChangeState(EnemyMovementStates.Disabled, 0);
             return;
         }
+        //makes sure to reset the time after each attack, so it doesn't re-enable mid-additional-attack
         ResetTime();
     }
 
-    public void ResetTime()
+    public void ResetTime() // can be called by other functions, just in case
     {
+        //mostly added specifically for the Physgun, so that it can reset the timer while in its Coroutine
         elapsed = 0;
     }
     public void ApplyKnockback(Vector3 knockback)
